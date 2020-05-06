fbb66055d171009971238813f35652d6ebed509d
Darkimage <luc-af@live.it>
2020-05-06 23:45:52 +0200

    Experimental add action to top toolbar

--- megascan_link/__init__.py
+++ megascan_link/__init__.py
@@ -3,6 +3,8 @@
 import sd 
 import time
 from PySide2 import QtCore
+from PySide2 import QtWidgets
+from PySide2.QtCore import Qt
 import megascan_link.socket as socket
 from queue import Queue
 import megascan_link.utilities as utilities
@@ -15,6 +17,8 @@ import ptvsd
 
 class Data(object):
     socketThread = None
+    toolbarAction = None
+    toolbar = None
 
 pluginData = Data()
 
@@ -28,6 +32,13 @@ def initializeSDPlugin():
     uiMgr = utilities.getUiManager()  
     # Get the main window to set the thread parent of
     mainWindow = uiMgr.getMainWindow()
+    print(mainWindow.findChildren(QtWidgets.QToolBar))
+    toolbars = mainWindow.findChildren(QtWidgets.QToolBar)
+    for toolbar in toolbars:
+        if mainWindow.toolBarArea(toolbar) == Qt.ToolBarArea.TopToolBarArea:
+            pluginData.toolbar = toolbar
+            pluginData.toolbarAction = toolbar.addAction("prova")
+            break
     pluginData.socketThread = socket.SocketThread(parent=mainWindow)
     importer = resImporter.ResourceImporter()
     receiver = socket.SocketReceiver(parent=mainWindow,importer=importer)
@@ -35,6 +46,8 @@ def initializeSDPlugin():
     pluginData.socketThread.onDataReceived.connect(receiver.onReceivedData, QtCore.Qt.QueuedConnection)
     pluginData.socketThread.start()
 
+
 def uninitializeSDPlugin(): 
     #stopping socket
     pluginData.socketThread.close()
+    pluginData.toolbar.removeAction(pluginData.toolbarAction)
