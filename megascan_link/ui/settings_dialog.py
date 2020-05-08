d6ab1b86c00624d0f873773369a39ff0a10d8268
Darkimage <luc-af@live.it>
2020-05-09 00:14:32 +0200

    Updated Settings Dialog

--- megascan_link/ui/settings_dialog.py
+++ megascan_link/ui/settings_dialog.py
@@ -152,7 +152,7 @@ class Ui_Dialog(object):
 
         self.retranslateUi(Dialog)
 
-        self.tabWidget.setCurrentIndex(1)
+        self.tabWidget.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(Dialog)
