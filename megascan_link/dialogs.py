c19520ea7ad99aeab945edacb9ffcc171c7e695a
Darkimage <luc-af@live.it>
2020-05-10 00:11:30 +0200

    Dialogs imports and doctrings Refactor

--- megascan_link/dialogs.py
+++ megascan_link/dialogs.py
@@ -1,23 +1,38 @@
-from PySide2 import QtCore
-from PySide2 import QtGui
-from PySide2.QtCore import Qt
-from PySide2 import QtWidgets
-from pathlib import Path
-import megascan_link
-from megascan_link import config
-from megascan_link.ui import settings_dialog
-from megascan_link.ui import import_dialog
-from megascan_link import sockets
+"""Module which contains all the dialogs used by the plugin
+
+The dialogs are generated using QtDesigner and are located under /megascan_lin/ui/uiDesign
+and then converted to python code using the buildDialogs.py script
+"""
+
 import importlib
+from pathlib import Path
+
+from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2.QtCore import Qt
+
+import megascan_link
+from megascan_link import config, sockets
+from megascan_link.ui import import_dialog, settings_dialog
+
 importlib.reload(settings_dialog)
 importlib.reload(import_dialog)
 
-class SelectPackageDialog(QtWidgets.QDialog, import_dialog.Ui_Dialog): 
+class SelectPackageDialog(QtWidgets.QDialog, import_dialog.Ui_Dialog):
+    """Dialogs displayed when an import is requested from Quixel Bridge
+
+    The user can select to which packages import the Megascan Assets or dismiss it
+    """
+    #: Subscribable signal emitted when the user close the dialog the param is set to the list of the selected packages
+    #: 
+    #: :type: QtCore.Signal
     returnValue = QtCore.Signal(object)
 
     def __init__(self,  packageList, parent=None,):
         super().__init__(parent=parent)
         self.setupUi(self)
+        #: List of currently selected packages the data value is set to point to the corresponding SDPackage Reference
+        #:
+        #: :type: List[QtWidgets.QListWidgetItem]
         self.selectedPackages = []
         self.packageWidget.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
         self.packageWidget.itemSelectionChanged.connect(self._getSelected)
@@ -36,6 +51,8 @@ class SelectPackageDialog(QtWidgets.QDialog, import_dialog.Ui_Dialog):
         self.cancelBtn.clicked.connect(lambda: self._returnFromDialog(True))
 
     def _getSelected(self):
+        """Internal method used to update the currently selected packages
+        """        
         self.selectedPackages.clear()
         packages = self.packageWidget.selectedItems()
         print(self.packageWidget.selectedItems())
@@ -44,6 +61,11 @@ class SelectPackageDialog(QtWidgets.QDialog, import_dialog.Ui_Dialog):
             print(package.data(Qt.UserRole))
     
     def _returnFromDialog(self, isCancel):
+        """Internal method used to dismiss the dialog
+
+        :param isCancel: True or False depending if the user clicked *Import* or *Cancel* on the dialog
+        :type isCancel: bool
+        """        
         if isCancel:
             self.close()
         else:
@@ -52,6 +74,8 @@ class SelectPackageDialog(QtWidgets.QDialog, import_dialog.Ui_Dialog):
 
 
 class SettingsDialog(QtWidgets.QDialog, settings_dialog.Ui_Dialog):
+    """Dialog displayed to the user for editing the plugin settingss
+    """    
 
     def __init__(self, socketRef: sockets.SocketThread, parent=None):
         self.config = config.ConfigSettings()
@@ -61,20 +85,25 @@ class SettingsDialog(QtWidgets.QDialog, settings_dialog.Ui_Dialog):
         self.setupUi(self)
         self.portNumber.setText(self.config.getConfigSetting("Socket", "port"))
         self.portNumber.setValidator(QtGui.QIntValidator(self))
-        self.portNumber.textChanged.connect(self.setNeedRestart)
+        self.portNumber.textChanged.connect(self._setNeedRestart)
         self.timeoutNumber.setText(self.config.getConfigSetting("Socket", "timeout"))
         self.timeoutNumber.setValidator(QtGui.QIntValidator(0, 60, self))
-        self.timeoutNumber.textChanged.connect(self.setNeedRestart)
+        self.timeoutNumber.textChanged.connect(self._setNeedRestart)
         self.createGraph.setCheckState(Qt.CheckState.Checked if self.config.checkIfOptionIsSet("General", "createGraph") else Qt.CheckState.Unchecked)
         self.importLODs.setCheckState(Qt.CheckState.Checked if self.config.checkIfOptionIsSet("3D Asset","importLODs") else Qt.CheckState.Unchecked)
         self.saveBtn.pressed.connect(self.saveSettings)
         self.cancelBtn.pressed.connect(lambda: self.close())
     
-    def setNeedRestart(self, changedStr):
+    def _setNeedRestart(self, changedStr):
+        """Internal method to set the need restart flag which is later passed to the socket thread
+        when the dialog is closed
+        """        
         print("Port or Timeout changed socket need to restart")
         self.needRestart = True
 
     def saveSettings(self):
+        """Saved the changed settings to file then inform the socket thread if it need to restart himself
+        """        
         self.config.updateConfigSetting("Socket", "port", self.portNumber.text(), False)
         self.config.updateConfigSetting("Socket", "timeout", self.timeoutNumber.text(), False)
         createGraphState = True if self.createGraph.checkState() == Qt.CheckState.Checked else False
@@ -82,5 +111,6 @@ class SettingsDialog(QtWidgets.QDialog, settings_dialog.Ui_Dialog):
         importLODsState = True if self.importLODs.checkState() == Qt.CheckState.Checked else False
         self.config.updateConfigSetting("3D Asset", "importLODs", importLODsState, False)
         self.config.flush()
-        self._sockRef.restart()
+        if self.needRestart:
+            self._sockRef.restart()
         self.close()
