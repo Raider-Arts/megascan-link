25ec19480f6404d6bff87e06f4aa4404fb2f8af5
Darkimage <luc-af@live.it>
2020-05-10 00:12:13 +0200

    Utilities imports and doctrings Refactor

--- megascan_link/utilities.py
+++ megascan_link/utilities.py
@@ -1,24 +1,35 @@
-import sd
-import sd.api.sdapplication as sdapp
+"""Module containing utilities function for general usage
+"""
 import os
 
-def getAbsCurrentPath(append):
+import sd
+from sd.api.sdapplication import SDApplication
+
+
+def getAbsCurrentPath(append: str) -> str:
+	"""Simple function to get the current script path
+
+	:param append: path or filename to add
+	:type append: str
+	:return: the full path plus the append param
+	:rtype: str
+	"""	
 	return os.path.join(os.path.abspath(os.path.split(__file__)[0]), append)
 
 
-def getApp() -> sdapp.SDApplication:
+def getApp() -> SDApplication:
 	"""Helper function to retrive the SDApplication
-	
-	Returns:
-		SDApplication -- Substance Designer sdapplication instance
-	"""	
+
+	:return: the substance designer application instance
+	:rtype: SDApplication
+	"""
 	context = sd.getContext()
 	return context.getSDApplication()
 
-def getUiManager(): 
+def getUiManager():
 	"""Helper function to retrive the QtPythonUIManager
-	
-	Returns:
-		QtForPythonUIMgr -- class instance
-	"""	
+
+	:return: return the current QtForPythonUIMgr instance 
+	:rtype: UIMngr
+	"""
 	return getApp().getQtForPythonUIMgr()
