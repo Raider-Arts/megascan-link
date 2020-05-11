0ea216d2392fdf9f00379e2c75bef2644b138245
Darkimage <luc-af@live.it>
2020-05-12 00:03:15 +0200

    Updated icon module to accept genereric icon filename a string Added help icon

--- megascan_link/icon.py
+++ megascan_link/icon.py
@@ -6,10 +6,15 @@ import sd
 from sd.api.sdtexture import SDTexture
 
 
-def getIcon():
-    """Return the path to the Megascan logo icon
-    """    
-    return os.path.join(os.path.abspath(os.path.split(__file__)[0]), 'megascan_logo.png')
+def getIcon(name: str) -> str:
+    """Return the path to the specified icon
+
+    :param name: icon filename
+    :type name: str
+    :return: absolute path to the icon
+    :rtype: str
+    """     
+    return os.path.join(os.path.abspath(os.path.split(__file__)[0]), name)
 
 class MegascanIcon(object):
     """Simple class for storing and retriving the Megascan Logo as a path to file or as a SDTexture instance
@@ -17,8 +22,8 @@ class MegascanIcon(object):
     #: Path to the Megascan Logo
     #:
     #: :type: str
-    path = getIcon()
+    path = getIcon('megascan_logo.png')
     #: SDTexture instance created form the Megascan Logo path
     #:
     #: :type: SDTexture
-    sdtexture = SDTexture.sFromFile(getIcon())
+    sdtexture = SDTexture.sFromFile(getIcon('megascan_logo.png'))
